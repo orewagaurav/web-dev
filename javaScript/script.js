8aefe444ef5962ead5cd36b60b1005d7e9291161
orewagaurav <gauravkumar47967@gmail.com>
2025-02-25 00:08:26 +0530

    js

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,5 +1,13 @@
 alert("Hello World");
 console.log("code is running");
 console.error("this is an error");
-var a =prompt("Enter your name");
-console.log("Your number is"+a);
+var a =prompt("Enter your number");
+
+var isTrue =prompt("Are you sure u want to exit?");
+if(isTrue){
+    console.log("exit successfully");
+}
+else{
+    console.log("exit failed");
+}
+console.log("Your number is " + a);
