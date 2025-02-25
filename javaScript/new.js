8cbb3a6477c5283620e5810c7f7c14145d0a8526
orewagaurav <gauravkumar47967@gmail.com>
2025-02-26 01:06:31 +0530

    js

--- javaScript/new.js
+++ javaScript/new.js
@@ -2,10 +2,76 @@ console.log("Hello World");
 console.log("code is running");
 console.error("this is an error");
 
+//variables
+//var is declared globally
 var a=10;
 var b=20;
 var c= 45;
 console.log("sum",a+b+c+5);
 console.log("sum"+(a+b+c+5));
 console.log("sum"+a+b+c+5);
+var name ="Gaurav";
+console.log(name);
+console.log(typeof name,typeof a);
 
+const pi=3.14;
+console.log(pi);
+
+//let is declared locally or for block scope
+let A=20;
+let B=30;
+let C=40;
+console.log("Sum of A,B,C",A+B+C);
+console.log("A is ",A);
+
+{
+    let A=50;
+    console.log(A);
+}
+console.log("A is ",A);
+
+//const is declared locally or for block scope
+const P=20;
+const Q=30;
+const R=40;
+console.log("sum of P,Q,R",P+Q+R);
+
+//data types
+//primitive data types
+//number
+//string
+//boolean
+//null
+//undefined
+//symbol
+
+let num=10.13;
+let str ="hello";
+const bool = true;
+let nullVar=null;
+let undefinedVar= undefined;
+// let symbolVar=symbol("Unique Identifier");
+
+console.log(num,str,bool,nullVar,undefinedVar);
+console.log(typeof num,typeof str,typeof bool,typeof nullVar,typeof undefinedVar);
+
+//non-primitive data types
+//object
+//array
+//function
+
+//object
+let person={
+    name:"Gaurav",
+    age:20,
+    "student id":4672,//it involves space so we use ""
+    isStudent:true
+}
+console.log(person);
+
+person.salary=88000
+person["student id"]=4678;
+person.isStudent=false;
+person.name="Raj";
+person.age=21;
+console.log(person);
