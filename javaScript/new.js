9a6d70c287961fca94428401d507667a7eaec9be
orewagaurav <gauravkumar47967@gmail.com>
2025-02-26 00:24:13 +0530

    node js

--- javaScript/new.js
+++ javaScript/new.js
@@ -1,3 +1,11 @@
 console.log("Hello World");
 console.log("code is running");
 console.error("this is an error");
+
+var a=10;
+var b=20;
+var c= 45;
+console.log("sum",a+b+c+5);
+console.log("sum"+(a+b+c+5));
+console.log("sum"+a+b+c+5);
+
